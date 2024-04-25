a332494a25a6c898ca3efac8c85f3639ab6c1638
Nikunj Navdiya <9723282128nnm@gmail.com>
2024-04-25 09:41:58 +0530

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,6 +7,19 @@ const App = () => {
 
   return(
     <div>
+      <h1>Give feedback</h1>
+      <hr/>
+      <button onClick={()=> {setGood(good+1)}}>Good</button>
+      <button onClick={()=> {setNeutral(neutral+1)}}>Neutral</button>
+      <button onClick={()=> {setBad(bad+1)}}>Bad</button>
+      <hr/>
+      <h1>Statistics</h1>
+      <p>Good:- <span>{good}</span></p>
+      <p>Neutral:- <span>{neutral}</span></p>
+      <p>Bad:- <span>{bad}</span></p>
+      <p>All:- <span>{good+neutral+bad}</span></p>
+      <p>Average:- <span>{((good*1)+(bad*(-1)))/(good+neutral+bad)}</span></p>
+      <p>Positive:- <span>{(good*100)/(good+neutral+bad)}</span></p>
     </div>
   )
 }
