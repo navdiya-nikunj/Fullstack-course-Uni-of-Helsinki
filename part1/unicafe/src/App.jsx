a363b191f5c70eb9cef41c4ce0d779a7d7d6a535
Nikunj Navdiya <9723282128nnm@gmail.com>
2024-04-25 10:25:29 +0530

    Exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,22 @@
 import { useState } from "react";
 
+
+const Statistics = ({good,neutral,bad}) => {
+  const all = good+neutral+bad;
+  const average = (good-bad)/all;
+  const positive = (good/all)*100;
+  return (<>
+    <h1>Statistics</h1>
+    <p>Good:- <span>{good}</span></p>
+    <p>Neutral:- <span>{neutral}</span></p>
+    <p>Bad:- <span>{bad}</span></p>
+    <p>All:- <span>{all}</span></p>
+    <p>Average:- <span>{average}</span></p>
+    <p>Positive:- <span>{positive}</span></p>
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -13,13 +30,7 @@ const App = () => {
       <button onClick={()=> {setNeutral(neutral+1)}}>Neutral</button>
       <button onClick={()=> {setBad(bad+1)}}>Bad</button>
       <hr/>
-      <h1>Statistics</h1>
-      <p>Good:- <span>{good}</span></p>
-      <p>Neutral:- <span>{neutral}</span></p>
-      <p>Bad:- <span>{bad}</span></p>
-      <p>All:- <span>{good+neutral+bad}</span></p>
-      <p>Average:- <span>{((good*1)+(bad*(-1)))/(good+neutral+bad)}</span></p>
-      <p>Positive:- <span>{(good*100)/(good+neutral+bad)}</span></p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
