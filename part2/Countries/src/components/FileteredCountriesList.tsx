25c9d80832782c0b04a16e5fdf4092cdf8f9993f
navdiya-nikunj <nikunj@cybersync-technologies.com>
2024-07-24 23:56:46 +0530

    Exercise 2.19

--- part2/Countries/src/components/FileteredCountriesList.tsx
+++ part2/Countries/src/components/FileteredCountriesList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import CountryDetails from "./Country";
 
 interface FilteredCountriesListProps {
@@ -10,14 +11,32 @@ interface Country {
 }
 
 const FilteredCountriesList = ({ countries }: FilteredCountriesListProps) => {
+  const [showCountry, setShowCountry] = useState<string | null>(null);
+  useEffect(() => {
+    setShowCountry(null);
+  }, [countries]);
+
   const length: number = countries.length;
   if (length > 10) {
     return <p>Too many matches, specify another filter</p>;
   } else if (length === 1) {
     return <CountryDetails countryname={countries[0].name.common} />;
+  } else if (showCountry !== null) {
+    return <CountryDetails countryname={showCountry} />;
   } else {
     return countries.map((country: any) => {
-      return <p>{country.name.common}</p>;
+      return (
+        <div>
+          <span>{country.name.common} </span>
+          <button
+            onClick={() => {
+              setShowCountry(country.name.common);
+            }}
+          >
+            Show
+          </button>
+        </div>
+      );
     });
   }
 };
